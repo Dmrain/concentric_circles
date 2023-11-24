efe8a6874a44daf1e20d6bab9df3f318201f6884
unknown <danya.masyanov@yandex.ru>
2023-11-24 15:44:34 +0400

    Fixed file main.py
    Added description for functions in the code

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 
 
+# функция add_circle() создает новую окружность с использованием
+# метода create_oval и добавляет ее на холст в центре.
 def add_circle():
     global radius
     circles.append(canvas.create_oval(center_x - radius, center_y - radius,
                                       center_x + radius, center_y + radius, outline="black"))
     radius -= min(hx, hy)
 
+# функция remove_circle() удаляет последнюю
+# нарисованную окружность с холста.
 def remove_circle():
     global radius
     if circles:
         canvas.delete(circles.pop())
         radius += min(hx, hy)
 
+# on_key_press(event) - функция-обработчик событий клавиатуры.
 def on_key_press(event):
     if event.keysym == 'Return':
         add_circle()
@@ -23,7 +28,6 @@ def on_key_press(event):
 
 root = tk.Tk()
 root.title('Концентрические окружности')
-
 width, height = 700, 450
 center_x, center_y = width // 2, height // 2
 radius = 200  # начальный радиус
